66a43bf8e785f81f5ce73c03911f8af774e6e87e
David Lynch <kemayo@gmail.com>
2016-09-06 00:35:12 -0500

    Worked out why per-point icon scaling wasn't working

--- handler.lua
+++ handler.lua
@@ -161,7 +161,7 @@ local get_point_info = function(point)
         elseif point.junk then
             category = "junk"
         end
-        return label, icon, category, point.quest, point.faction
+        return label, icon, category, point.quest, point.faction, point.scale
     end
 end
 local get_point_info_by_coord = function(mapFile, coord)
@@ -393,8 +393,8 @@ do
         local state, value = next(t, prestate)
         while state do -- Have we reached the end of this zone?
             if value and ns.should_show_point(state, value, currentZone, currentLevel) then
-                local label, icon = get_point_info(value)
-                local scale = (point and point.scale or 1) * (icon and icon.scale or 1) * ns.db.icon_scale
+                local label, icon, _, _, _, scale = get_point_info(value)
+                scale = (scale or 1) * (icon and icon.scale or 1) * ns.db.icon_scale
                 return state, nil, icon, scale, ns.db.icon_alpha
             end
             state, value = next(t, state) -- Get next data
