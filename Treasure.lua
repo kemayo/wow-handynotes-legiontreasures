6b15decba42dc6fe0452996c06fd326017363ad0
David Lynch <kemayo@gmail.com>
2016-09-07 00:06:47 -0500

    Amend my grapple helper so I can add notes if needed, also add a note.

--- Treasure.lua
+++ Treasure.lua
@@ -8,15 +8,22 @@ local CHEST = 'Treasure Chest'
 local CHEST_SM = 'Small Treasure Chest'
 local CHEST_GLIM = 'Glimmering Treasure Chest'
 local REQ_GRAPPLE = 'Requires: Stormforged Grapple Launcher'
-local path = function(questid, label, atlas, scale)
+local path = function(questid, label, atlas, note, scale)
     label = label or "Path to treasure"
     atlas = atlas or "map-icon-SuramarDoor.tga" -- 'PortalPurple'
-    return {quest=questid, label=label, atlas=atlas, path=true, scale=scale or 1}
+    return {
+        quest = questid,
+        label = label,
+        atlas = atlas,
+        path = true,
+        scale = scale,
+        note = note,
+    }
 end
 ns.path = path
-local grapple = function(questid)
+local grapple = function(questid, note)
     -- 'Vehicle-SilvershardMines-Arrow'
-    return path(questid, "Grapple start point", 'MiniMap-DeadArrow', 1.5)
+    return path(questid, "Grapple start point", 'MiniMap-DeadArrow', note, 1.5)
 end
 ns.grapple = grapple
 
@@ -196,7 +203,7 @@ ns.points = {
         [75164949] = {quest=42629, currency=ARTIFACT, label=CHEST, note="On top of the mast"},
         [75676060] = {quest=43304, currency=ARTIFACT, label=CHEST_SM, note=REQ_GRAPPLE},
         [78427138] = {quest=43307, currency=ARTIFACT, label=CHEST, note="*Really* requires the Stormforged Grapple Launcher"},
-        [75846406] = grapple(43307),
+        [75846406] = grapple(43307, "Route *requires* taking some falling damage, I think."),
         [81876750] = {quest=40099, currency=ARTIFACT, label=CHEST},
         [82405451] = {quest=43191, currency=ARTIFACT, label=CHEST_SM},
     },
