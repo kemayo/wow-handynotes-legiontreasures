ec52bc7cac92842e98efead4a8164cc349b37101
David Lynch <kemayo@gmail.com>
2020-12-10 21:36:09 -0600

    Improve display of Dog's pebble

--- Treasure.lua
+++ Treasure.lua
@@ -503,20 +503,22 @@ ns.RegisterPoints(882, { -- MacAree
     [37102010] = {quest=49264, label="Void-Seeped Cache", junk=true, currency=ARGUNITE},
 })
 -- Small zones
+
+local pebble = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526, upcoming=false, atlas="islands-azeritechest",scale=1.2,}
 ns.RegisterPoints(627, { -- Dalaran
     [28466450] = {quest=41929, loot={7676}, label="Desmond's Lockbox", note="Locked"},
     [47404120] = {quest=45365, loot={143534}, toy=true, note="On the table on the second floor of the Legerdemain Lounge", level=10},
     -- Dog pebble, questid is for showing Dog the pebble, not looting it
-    [38102920] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [42104440] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [41005320] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [46205390] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [49406940] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [51706220] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [54505320] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [54304080] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [47702920] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
-    [44601820] = {quest=46952, loot={147420}, note="Show to Dog in your Draenor garrison", hide_before=30526},
+    [38102920] = pebble,
+    [42104440] = pebble,
+    [41005320] = pebble,
+    [46205390] = pebble,
+    [49406940] = pebble,
+    [51706220] = pebble,
+    [54505320] = pebble,
+    [54304080] = pebble,
+    [47702920] = pebble,
+    [44601820] = pebble,
 })
 ns.RegisterPoints(642, { -- Darkpens, Val'sharah
     [42018849] = {quest=39085, --[[currency=ARTIFACT,]] label=CHEST_SM, note="In water at bottom of stairs"},
